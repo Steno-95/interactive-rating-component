85dc1a6490d7372b45d43cef4fcdaa6fe8de5601
Steno-95 <lezzistefano95@gmail.com>
2025-05-07 12:36:07 +0200

    Add: submit logic and display the submitted value

--- src/components/ThankYouCard/TYCard.jsx
+++ src/components/ThankYouCard/TYCard.jsx
@@ -2,11 +2,11 @@ import TYButton from "./TYButton";
 import TYIcon from "./TYIcon";
 import TYTextBox from "./TYTextBox";
 
-function TYCard({ onReset }) {
+function TYCard({ onReset, rating }) {
   return (
     <article className="card justify-center items-center px-5 py-6 gap-5">
       <TYIcon />
-      <TYButton onReset={onReset} />
+      <TYButton onReset={onReset} rating={rating} />
       <TYTextBox />
     </article>
   );
