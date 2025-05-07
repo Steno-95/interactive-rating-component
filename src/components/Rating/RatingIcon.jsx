c51d3590bedc12db3eb46cc47f547b58c52e1909
Steno-95 <lezzistefano95@gmail.com>
2025-05-07 11:49:13 +0200

    Add: outline styles on on focus for btns

--- src/components/Rating/RatingIcon.jsx
+++ src/components/Rating/RatingIcon.jsx
@@ -1,7 +1,7 @@
 function RatingIcon() {
   return (
     <button
-      className="btn btn-radio size-8 flex items-center justify-center"
+      className="btn btn-radio size-8 flex items-center justify-center cursor-default"
       aria-label="icon of a star"
     >
       <img src="/images/icon-star.svg" alt="icon of a star" className="w-fit" />
--- src/components/Rating/RatingSubmitBtn.jsx
+++ src/components/Rating/RatingSubmitBtn.jsx
@@ -2,7 +2,7 @@ function RatingSubmitBtn() {
   return (
     <button
       type="submit"
-      className="btn text-(--grey-dark) font-bold text-[.9rem] tracking-widest bg-(--primary) w-full pt-2 pb-1 cursor-pointer hover:bg-(--white) "
+      className="btn text-(--grey-dark) font-bold text-[.9rem] tracking-widest bg-(--primary) w-full pt-2 pb-1 cursor-pointer hover:bg-(--white) focus:outline-(--white)"
     >
       SUBMIT
     </button>
