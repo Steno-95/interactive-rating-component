a0d7de71ed446ef1668176539c195978aaccc8c8
Steno-95 <lezzistefano95@gmail.com>
2025-05-07 16:37:52 +0200

    Fix: display the footer of the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import RatingCard from "./components/Rating/RatingCard";
 import TYCard from "./components/ThankYouCard/TYCard";
+import Footer from "./ui/Footer";
 
 function App() {
   const [submitted, setSubmitted] = useState(false);
@@ -19,6 +20,7 @@ function App() {
     <main className="min-h-screen flex flex-col">
       {!submitted && <RatingCard onSubmit={handleOnSubmit} />}
       {submitted && <TYCard onReset={handleReset} rating={rating} />}
+      <Footer />
     </main>
   );
 }
--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -1,9 +1,9 @@
 function Footer() {
   return (
-    <footer className="text-center text-[1rem] md:text-[1.5rem] text-[var(--outfocus-color)] mt-10 min-w-[fit]">
+    <footer className="text-center text-[.7rem] text-(--grey) mt-10 min-w-[fit]">
       Challenge by{" "}
       <a
-        className="text-blue-600 hover:text-(--active-text) focus:text-(--active-text)"
+        className="text-orange-200 hover:text-(--primary) focus:text-(--primary)"
         href="https://www.frontendmentor.io?ref=challenge"
         target="_blank"
       >
@@ -11,7 +11,7 @@ function Footer() {
       </a>
       . Coded by{" "}
       <a
-        className="text-blue-600 hover:text-(--active-text) focus:text-(--active-text)"
+        className="text-orange-200 hover:text-(--primary) focus:text-(--primary)"
         href="https://github.com/Steno-95"
         title="The link will redirect to the github of Stefano Lezzi"
       >
