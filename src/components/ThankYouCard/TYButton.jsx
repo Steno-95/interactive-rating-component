9c16efb2493c1a8e3be3bf10a525c2b75935ba9b
Steno-95 <lezzistefano95@gmail.com>
2025-05-07 11:01:33 +0200

    Fix: adjust the layout of TYButton

--- src/components/ThankYouCard/TYButton.jsx
+++ src/components/ThankYouCard/TYButton.jsx
@@ -1,10 +1,10 @@
 function TYButton({ onReset }) {
   return (
     <button
-      className="text-(--primary) btn btn-radio w-fit flex items-center justify-center p-3"
+      className="text-(--primary) btn btn-radio w-fit flex items-center justify-center px-3  "
       onClick={onReset}
     >
-      You selected 4 out of 5
+      <span className="pt-1">You selected 4 out of 5</span>
     </button>
   );
 }
